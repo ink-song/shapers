4dfb00e50b20471bf999f13799c36a154253ab9a
ink-song <ink-song@users.noreply.github.com>
2021-05-11 08:00:55 +0000

    deploy: 21d340a2354995ae62c57c976348898b3258db6b

--- index_props.js
+++ index_props.js
@@ -8,7 +8,7 @@ export default {
     'outputPath': "index.html",
     'title': undefined,
     'content': React.createElement("article", { dangerouslySetInnerHTML: {
-            __html: '<p><img src="./images/tobeaman.png" alt="to be a man"></p>\n<pre><code>-风吹落枯叶，枯叶滋养土壤，肥沃的土壤帮助果实，缓慢而坚定的生长\n</code></pre>'
+            __html: '<p><img src="./images/tobeaman.png" alt="to be a man"></p>\n<pre><code>风吹落枯叶，枯叶滋养土壤，肥沃的土壤帮助果实，缓慢而坚定的生长\n</code></pre>'
         } }),
     'head': React.createElement(React.Fragment, null,
         React.createElement("link", { crossOrigin: "anonymous", href: "https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css", integrity: "sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X", rel: "stylesheet" })),
@@ -18,7 +18,7 @@ export default {
         React.createElement("script", { src: "/shapers/index.js", type: "module" })),
     'contentTitle': undefined,
     'contentBody': React.createElement("article", { dangerouslySetInnerHTML: {
-            __html: '<p><img src="./images/tobeaman.png" alt="to be a man"></p>\n<pre><code>-风吹落枯叶，枯叶滋养土壤，肥沃的土壤帮助果实，缓慢而坚定的生长\n</code></pre>'
+            __html: '<p><img src="./images/tobeaman.png" alt="to be a man"></p>\n<pre><code>风吹落枯叶，枯叶滋养土壤，肥沃的土壤帮助果实，缓慢而坚定的生长\n</code></pre>'
         } }),
     'toc': null,
     'author': "ink-song",
@@ -27,8 +27,8 @@ export default {
         "shaper"
     ],
     'date': "2021-03-05T01:33:36.000Z",
-    'updated': "2021-05-11T06:53:09.000Z",
-    'excerpt': "-风吹落枯叶，枯叶滋养土壤，肥沃的土壤帮助果实，缓慢而坚定的生长 ",
+    'updated': "2021-05-11T07:58:55.000Z",
+    'excerpt': "风吹落枯叶，枯叶滋养土壤，肥沃的土壤帮助果实，缓慢而坚定的生长 ",
     'cover': "./images/tobeaman.png",
     'sidebar': [
         {
